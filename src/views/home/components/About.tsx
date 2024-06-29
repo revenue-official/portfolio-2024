9579f4853aa4e72199b2fb67bb4e8bcce7fc4a1e
revenue-official <akunku5521@gmail.com>
2024-06-30 05:48:36 +0700

    Auto commit

--- src/views/home/components/About.tsx
+++ src/views/home/components/About.tsx
@@ -12,7 +12,12 @@ export default function About({ sectionRef }: { sectionRef: any }) {
 				<UserRound className="h-6 w-6 md:h-7 md:w-7" />
 			</Title>
 			<div className="w-full gap-8 md:flex">
-				<div className="rounded-3xl shadow-lg  md:h-1/2 md:w-1/2">
+				<div className="relative mx-auto h-60 w-60 overflow-hidden rounded-3xl shadow-lg md:h-1/2 md:w-1/2">
+					<div className="absolute -left-20 top-5 z-10 flex w-60 -rotate-45 items-center justify-center bg-gradient-to-b from-yellow-600 to-yellow-500 shadow-md md:-left-24 md:top-4">
+						<span className="text-nowrap py-0.5 font-poppins text-lg font-bold text-yellow-50 md:text-sm">
+							Profile
+						</span>
+					</div>
 					<Image
 						src={
 							"https://plus.unsplash.com/premium_photo-1674777843203-da3ebb9fbca0?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -23,28 +28,19 @@ export default function About({ sectionRef }: { sectionRef: any }) {
 						className="h-full w-full rounded-3xl object-cover"
 					/>
 				</div>
-				<div className="h-fit w-full rounded-3xl bg-zinc-50 px-8 py-4 shadow-lg dark:bg-dark-100">
-					<h1 className="h-10 w-full text-nowrap font-poppins text-base font-bold text-zinc-800 dark:text-zinc-200 md:text-3xl">
-						Hi, I am a full-stack developer.
+				<div className="min-h-20 w-full rounded-3xl px-8 pt-8 shadow-md md:bg-light-100 md:dark:bg-dark-100">
+					<h1 className="w-full text-nowrap text-center font-poppins text-lg font-bold text-zinc-800 dark:text-zinc-200 md:h-10 md:text-2xl">
+						Hi, I am Teguh
 					</h1>
-					<span className="font-poppins text-xs text-zinc-500">
-						2020 - 2024
-					</span>
 					<div className="flex h-full w-full flex-col">
-						<p className="text-md zinc-600 w-full pt-4 indent-8 font-poppins text-base dark:text-zinc-400">
+						<p className="text-md w-full pt-4 text-center font-roboto text-zinc-600 dark:text-zinc-400">
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
 							quasi. Voluptatum, optio! Odit voluptatum, excepturi molestiae
 							molestias corrupti, error, doloremque officiis dignissimos nobis
 							voluptate laudantium minus dolore harum fuga et! Lorem ipsum dolor
 							sit, amet consectetur adipisicing elit. Optio, quas!
 						</p>
-						<p className="text-md zinc-600 w-full pt-4 indent-8 font-poppins text-base dark:text-zinc-400">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
-							quasi. Voluptatum, optio! Odit voluptatum, excepturi molestiae
-							molestias corrupti, error, doloremque officiis dignissimos nobis
-							voluptate laudantium minus dolore harum fuga et!
-						</p>
-						<p className="text-md zinc-600 w-full pt-4 indent-8 font-poppins text-base dark:text-zinc-400">
+						<p className="text-md w-full pt-4 text-center font-roboto text-zinc-600 dark:text-zinc-400">
 							Voluptatum, optio! Odit voluptatum, excepturi molestiae molestias
 							corrupti, error, doloremque officiis dignissimos nobis voluptate.
 						</p>
--- src/views/home/components/Title.tsx
+++ src/views/home/components/Title.tsx
@@ -8,14 +8,14 @@ interface TitleProps {
 
 export default function Title({ title, subtitle, children }: TitleProps) {
   return (
-    <div className="my-4 flex flex-col">
+    <div className="my-4 flex w-full flex-col items-center">
       <div className="inline-flex items-center gap-2 text-neutral-800 dark:text-neutral-200">
         {children}
-        <span className="font-poppins text-xl font-bold md:text-3xl">
+        <span className="font-poppins text-xl font-bold md:text-2xl">
           {title}
         </span>
       </div>
-      <span className="font-quicksand text-lg font-medium text-neutral-600 dark:text-neutral-400">
+      <span className="font-quicksand font-medium text-neutral-600 dark:text-neutral-400 md:text-base">
         {subtitle}
       </span>
     </div>
