06a2999ee84c36bf815d5341e3be561a34bef154
revenue-official <akunku5521@gmail.com>
2024-06-30 10:36:39 +0700

    Auto commit

--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -5,7 +5,7 @@ import Sidebar from "./Sidebar";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex max-h-screen bg-zinc-100 dark:bg-zinc-950">
+    <main className="flex bg-zinc-100 dark:bg-zinc-950">
       <Sidebar />
       <div className="h-screen w-full px-0 md:px-32">
         <div className="hidden-scrollbar flex h-full flex-col overflow-y-auto">
